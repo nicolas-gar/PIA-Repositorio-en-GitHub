19c4bcfbdc121e1b028c5c7c9ffa4ff7f70c8bdb
nicolas-gar <123913377+nicolas-gar@users.noreply.github.com>
2023-05-30 01:57:26 -0600

    Update PROBANDO ENVÍOS EN IDLE.py

--- Envio de Correos/PROBANDO ENVÍOS EN IDLE.py
+++ Envio de Correos/PROBANDO ENVÍOS EN IDLE.py
@@ -1,10 +1,1 @@
-#Nombre: Nicolás Alberto Graza Galicia
-#Matricula: 1998776
 
-import smtplib
-conn = smtplib.SMTP('smtp.gmail.com',587)
-conn.ehlo()
-conn.starttls()
-conn.login('alfperez3@gmail.com','jsmf ccak daiu cynu')
-conn.sendmail('alfperez3@gmail.com','aaron28perez@gmail.com','Subject: TestPractica10\n\nHola\n\n Prueba de 1993809- Aaron Perez Esparza' )
-conn.quit()
